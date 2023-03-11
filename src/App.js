a292861bf547be52e8cc5c015c189289d3c615fd
captaiiinsolo <solo.d.santos@gmail.com>
2023-03-10 21:11:38 -0800

    create pages and use react-router-dom to connect them to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom.min.css';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,7 +15,18 @@ import Footer from './components/Footer';
 function App() {
   return (
    <div className="App">
-    <Header></Header>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          {/* <Navigate to="/" /> */}
+        </Routes>
+      </Router>
+    </>
 
     <Footer></Footer>
    </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function Navigation() {
-    return (
-        <Navbar bg="primary" variant='dark' sticky='top' className='py-3'>
-            <Container fluid>
-                <Navbar.Brand className='justify-content-between'>Solomon Santos | Full Stack Developer</Navbar.Brand>
-                <Nav>
-                    <Nav.Link className='text-light' href="#home">Home</Nav.Link>
-                    <Nav.Link className='text-light' href="#projects">Projects</Nav.Link>
-                    <Nav.Link className='text-light' href="#about">About</Nav.Link>
-                    <Nav.Link className='text-light' href="#contact">Contact</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar bg="primary" variant="dark" sticky="top" className="py-3">
+      <Container fluid>
+        <Navbar.Brand className="justify-content-between">
+          Solomon Santos | Full Stack Web Developer
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link as={Link} to="/" className="text-light">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/projects" className="text-light">
+            Projects
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about" className="text-light">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" className="text-light">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
